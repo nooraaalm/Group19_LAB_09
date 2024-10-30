9e760cb6d14c1c43036c055dd5154922d9e1cd34
Noor Alam <nooralam.shine2017@gmail.com>
2024-10-31 04:51:08 +0530

    Calling function in the main function

--- main.c
+++ main.c
@@ -86,3 +86,11 @@ void WAVEFORM(void)                                       // DEFINE ANALOG WAVEF
     delay(1000);                                          // DELAY TO CONTROL WAVEFORM FREQUENCY
     }
 }
+int main(void)
+{
+    systick_setting();                                    // SYSTICK SETUP
+    while (1)
+    {
+    WAVEFORM();                                           // WAVEFORM
+    }
+}
